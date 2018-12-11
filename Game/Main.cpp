b2954329d82bd0c188bab6352e3972151732abf7
YdeaGames <ydeagames@gmail.com>
2018-12-11 09:30:32 +0900

    メモリリーク検知

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,7 +1,13 @@
 ﻿#include "Game.h"
+#include "MemoryLeakDetector.h"
 
 int main()
 {
+	// メモリーリークの検出
+#ifdef _DEBUG
+	SetUpMemoryLeakDetector();
+#endif
+
 	// 今回はC言語のrand関数を使用
 	std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
